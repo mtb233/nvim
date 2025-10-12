aaf667526fa7b69b1e1842b3a907d08edba67eb8
mtb233 <mb7843841056@hotmail.com>
2025-10-12 02:55:35 -0400

    Tab stops.

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
@@ -43,6 +43,7 @@ local telescope_builtin = require("telescope.builtin")
 
 -- Commands
 vim.cmd("set number")
+vim.cmd("set tabstop=2 softtabstop=0 shiftwidth=2 expandtab")
 vim.cmd.colorscheme "kanagawa-paper"
 vim.cmd("Neotree filesystem reveal right")
 
