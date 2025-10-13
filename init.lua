f38b23a020b4a3a173e0c8d0906e103331b7a708
mtb233 <mb7843841056@hotmail.com>
2025-10-12 22:28:12 -0400

    LSP autocompletion.

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Set leader first and foremost.
 vim.g.mapleader = " "
 
 -- Bootstrap lazy.nvim
@@ -38,6 +39,18 @@ require("lazy").setup({
 
 -- LSP
 vim.lsp.enable({"lua_ls", "clangd"})
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.keymap.set('i', '<C-Space>', function()
+        vim.lsp.completion.get()
+      end)
+    end
+  end,
+})
 
 local telescope_builtin = require("telescope.builtin")
 
@@ -51,5 +64,8 @@ vim.cmd("Neotree filesystem reveal right")
 vim.keymap.set("n", "<leader>f", ":Neotree filesystem reveal right<CR>")
 vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
 vim.keymap.set("n", "<leader>g", telescope_builtin.live_grep, {})
-vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>D", vim.diagnostic.open_float, {})
+vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
